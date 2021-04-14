d3c41084f494c7a6382590344451822befbc0939
Otávio Cipriano <otavio.b.cipriano@outlook.com>
2021-04-14 02:55:06 -0300

    fixed procfile again

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         current_time =str(datetime.utcnow().time())[:5]
         message = ''
         
-        if current_time == '05:52':
+        if current_time == '05:58':
 
             jobs = find_jobs()
         
